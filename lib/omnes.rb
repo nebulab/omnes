5d027c1ec8bc413c22c86cfc2a927b66f6986e60
Marc Busqué <marc@lamarciana.com>
2022-03-17 08:32:45 +0100

    Make components configuration accessible at the top level

--- lib/omnes.rb
+++ lib/omnes.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
-require "omnes/version"
 require "omnes/bus"
+require "omnes/event"
+require "omnes/subscriber"
+require "omnes/version"
 
 # Pub/sub bus behavior
 #
 # Include this module to have a class work as an {Omnes::Bus}.
 module Omnes
+  # Shortcut to access components configuration
+  #
+  # @return [Omnes::Config]
+  def self.config
+    Config
+  end
+
+  # Wrapper for components configuration
+  module Config
+    # {Omnes::Subscriber} configuration
+    #
+    # @return [Dry::Configurable::Config]
+    def self.subscriber
+      Omnes::Subscriber.config
+    end
+
+    # {Omnes::Event} configuration
+    #
+    # @return [Dry::Configurable::Config]
+    def self.event
+      Omnes::Event.config
+    end
+  end
+
   def self.included(klass)
     bus = Bus.new(caller_location_start: 2)
     klass.define_method(:omnes_bus) { bus }
--- spec/unit/omnes_spec.rb
+++ spec/unit/omnes_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Omnes do
   subject { Class.new.include(described_class) }
 
   include_examples "bus"
+
+  describe ".config.subscriber" do
+    it "returns Omnes::Subscriber.config" do
+      expect(
+        described_class.config.subscriber
+      ).to be(Omnes::Subscriber.config)
+    end
+  end
+
+  describe ".config.event" do
+    it "returns Omnes::Event.config" do
+      expect(
+        described_class.config.event
+      ).to be(Omnes::Event.config)
+    end
+  end
 end
